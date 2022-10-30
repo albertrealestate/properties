42193916197635ee34d11f25eb13010dd2ed3498
albertrealestate <albertayoub@hotmail.com>
2022-10-30 00:38:55 -0400

    changed maisons to properties

--- deployChanges.py
+++ deployChanges.py
@@ -1,4 +1,4 @@
 import os
 
-os.system("git pull & git add . & git commit -m zz & git push & ng build --prod --base-href https://ayoubsaade.github.io/maisons/ & npx ngh --dir=dist/maisons")
+os.system("git pull & git add . & git commit -m zz & git push & ng build --prod --base-href https://albertrealestate.github.io/properties/ & npx ngh --dir=dist/properties")
 input()
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to maisons!');
+    expect(page.getTitleText()).toEqual('Welcome to properties!');
   });
 
   afterEach(async () => {
